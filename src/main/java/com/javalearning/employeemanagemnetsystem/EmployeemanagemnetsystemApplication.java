d7684ba70f27c7e1fa1a445d03acfd91a6021d03
1m-C00l <nawmayjue2013@gmail.com>
2025-11-23 17:31:26 +0630

    :sparkles: feat: added codes

---
+++ src/main/java/com/javalearning/employeemanagemnetsystem/EmployeemanagemnetsystemApplication.java
@@ -0,0 +1,13 @@
+package com.javalearning.employeemanagemnetsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeemanagemnetsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeemanagemnetsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalearning/employeemanagemnetsystem/EmployeemanagemnetsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearning.employeemanagemnetsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeemanagemnetsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
